365364fa49d277a877214a39b53c9f25ce7ea75c
Remco Gerlich <remco.gerlich@nelen-schuurmans.nl>
2022-03-07 16:36:48 +0100

    New setupProxy; fix TS complaint

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,30 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  const password = process.env.PROXY_PASSWORD;
-  const username = process.env.PROXY_USERNAME;
+module.exports = function(app) {
+  const env_prefix = process.env.PROXY_PREFIX || 'PROXY';
+  const url = process.env[env_prefix + '_URL'] || 'https://nxt3.staging.lizard.net/';
+  const apiKey = process.env[env_prefix + '_API_KEY'];
 
-  if (!password || !username) {
-    console.log("Please set PROXY_PASSWORD and PROXY_USERNAME variables!");
-    process.exit(1);
+  let proxyMiddleware;
+  console.log(`Proxying to ${url}.`);
+
+  if (!apiKey) {
+    console.log('Without authentication.');
+    proxyMiddleware = createProxyMiddleware({
+      target: url,
+      changeOrigin: true,
+    });
+  } else {
+    // Use HTTP basic auth, works with Lizard only
+    proxyMiddleware = createProxyMiddleware({
+      target: url,
+      changeOrigin: true,
+      auth: `__key__:${apiKey}`,
+    });
   }
 
-  const proxyMiddleware = createProxyMiddleware({
-    //    target: 'https://nxt3.staging.lizard.net/',
-    target: "https://parramatta.lizard.net/",
-    changeOrigin: true,
-    // Use HTTP basic auth, works with Lizard only
-    auth: `${username}:${password}`,
-  });
-
-  app.use("/api", proxyMiddleware);
-  app.use("/bootstrap", proxyMiddleware);
-  app.use("/wms", proxyMiddleware);
-  app.use("/proxy", proxyMiddleware);
+  app.use('/api', proxyMiddleware);
+  app.use('/bootstrap', proxyMiddleware);
+  app.use('/wms', proxyMiddleware);
+  app.use('/proxy', proxyMiddleware);
 };
