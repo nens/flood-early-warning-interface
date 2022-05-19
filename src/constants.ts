aacca57d44c8ef83d648ba4631e6d8e89ce2342d
Remco Gerlich <remco.gerlich@nelen-schuurmans.nl>
2022-05-19 15:06:11 +0200

    Remco handle defaults (#67)
    * Put defaults in a constant
    
    * Refactor because fields that are not optional anymore now aren't null or undefined

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,90 @@
+import { Config } from "./types/config";
+
+export const DEFAULT_CONFIG: Config = {
+  version: 1,
+  dashboardTitle: "FloodSmart dashboard",
+  boundingBoxes: {
+    default: [
+      // Parramatta's
+      "150.945110",
+      "-33.796767",
+      "151.072826",
+      "-33.743612",
+    ],
+  },
+  rasters: {
+    // Parramatta's. Need to be made optional, see #62.
+    rainForecast: "6ccb42ce-4e97-4376-b010-1b76a57b5253",
+    operationalModelDepth: "fd8efbb8-2acd-4736-90aa-1c09b1f810e6",
+    operationalModelLevel: "9fe0d03b-5d2a-4da4-8082-4aeff037fff5",
+  },
+  // The only key of this that is used is 'buildingsForFloodModelMap' -- make more generic
+  // See ticket #66.
+  wmsLayers: {},
+  // Parramatta's rain legend, probably decent enough as a default.
+  rainLegend: [
+    ["<10 mm", "#FFFFFF"],
+    ["10-20 mm", "#7DAED0"],
+    ["20-30 mm", "#499ED7"],
+    ["30-50 mm", "#2F82B3"],
+    ["50-100 mm", "#BDB23B"],
+    ["100-200 mm", "#EEA819"],
+    ["200-300 mm", "#E75339"],
+    ["300-500 mm", "#BE527D"],
+    [">500 mm", "#9B59B6"],
+  ],
+  rainfallWmsLayers: [],
+  rainPopupFields: [],
+  mapbox_access_token: "",
+  timeseriesForWarningAreas: {},
+  dams: {
+    type: "FeatureCollection",
+    features: [],
+  },
+  tiles: [],
+  flood_warning_areas: {
+    type: "FeatureCollection",
+    features: [],
+  },
+  trainingDashboards: [],
+  /* Set explicitly to show it is not missing. Undefined means not fixed to a specific moment. */ nowDateTimeUTC:
+    undefined,
+  fakeData: {},
+  rssUrl: "",
+  extraRasters: {
+    title: "",
+    maps: {},
+  },
+  tabs: [
+    {
+      url: "alarms",
+      title: "FloodSmart Warnings",
+    },
+    {
+      url: "damalarms",
+      title: "Dam Alarms",
+    },
+    {
+      url: "waterlevel",
+      title: "Flood Model",
+    },
+    {
+      url: "rainfall",
+      title: "Rainfall",
+    },
+    {
+      url: "stations",
+      title: "Stations & Graphs",
+    },
+    {
+      url: "issuedwarnings",
+      title: "Issued Warnings",
+    },
+  ],
+  infoImage: "",
+  infoText: "",
+};
+
 export function getMapBackgrounds(access_token: string) {
   return [
     {
